37632d804815a1044a6388e43f29df45775e3322
Arsalan Hussain <arsalanmhussain32@gmail.com>
2022-11-23 16:01:44 +0500

    added color toggle

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,60 @@
-import { type AppType } from "next/app";
-
 import { trpc } from "../utils/trpc";
-
 import "../styles/globals.css";
+import type { AppProps, AppContext } from "next/app";
+import NextApp from "next/app";
+import Head from "next/head";
+import type { ColorScheme } from "@mantine/core";
+import { ColorSchemeProvider, MantineProvider } from "@mantine/core";
+import { useState } from "react";
+import { getCookie, setCookie } from "cookies-next";
 
-const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+const MyApp = (props: AppProps & { colorScheme: ColorScheme }) => {
+  const { Component, pageProps } = props;
+
+  const [colorScheme, setColorScheme] = useState<ColorScheme>(
+    props.colorScheme
+  );
+
+  const toggleColorScheme = (value?: ColorScheme) => {
+    const nextColorScheme =
+      value || (colorScheme === "dark" ? "light" : "dark");
+    setColorScheme(nextColorScheme);
+    setCookie("mantine-color-scheme", nextColorScheme, {
+      maxAge: 60 * 60 * 24 * 30,
+    });
+  };
+  return (
+    <>
+      <Head>
+        <title>Mantine next example</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+        <link rel="shortcut icon" href="/favicon.svg" />
+      </Head>
+
+      <ColorSchemeProvider
+        colorScheme={colorScheme}
+        toggleColorScheme={toggleColorScheme}
+      >
+        <MantineProvider
+          theme={{ colorScheme }}
+          withGlobalStyles
+          withNormalizeCSS
+        >
+          <Component {...pageProps} />
+        </MantineProvider>
+      </ColorSchemeProvider>
+    </>
+  );
 };
+MyApp.getInitialProps = async (appContext: AppContext) => {
+  const appProps = await NextApp.getInitialProps(appContext);
 
+  return {
+    ...appProps,
+    colorScheme: getCookie("mantine-color-scheme", appContext.ctx) || "dark",
+  };
+};
 export default trpc.withTRPC(MyApp);
