1779f2b4d3ede3e7e51781ec16b1f8cf905662ad
Majid-Arab <arabmajid12@gmail.com>
2022-12-05 15:23:33 +0500

    datatable Pagination

--- src/server/trpc/router/siteinfo.ts
+++ src/server/trpc/router/siteinfo.ts
@@ -13,29 +13,39 @@ export const siteInfoRouter = router({
   getAll: publicProcedure.query(({ ctx }) => {
     return ctx.prisma.site_info.findMany();
   }),
-  // paginated: publicProcedure
-  // .input(z.object({
-  //   limit: z.number().min(1).max(100).nullish(),
-  //   cursor: z.number().nullish(), // <-- "cursor" needs to exist, but can be any type
-  // }))    .query( async ({ input }) => {
-  //   const limit = input.limit ?? 50;
-  //   const { cursor } = input;
-  //   const items = await prisma?.site_info.findMany({
-  //     take: limit + 1, // get an extra item at the end which we'll use as next cursor
-  
-  //     cursor: cursor ? { myCursor: cursor } : undefined,
-  //     orderBy: {
-  //       myCursor: 'asc',
-  //     },
-  //   })
-  //   let nextCursor: typeof cursor | undefined = undefined;
-  //   if (items!.length > limit) {
-  //     const nextItem = items!.pop()
-  //     nextCursor = nextItem!.myCursor;
-  //   }
-  //   return {
-  //     items,
-  //     nextCursor,
-  //   };
-  // })
+  paginated:   publicProcedure
+  .input(z.object({
+    limit: z.number().min(1).max(100).nullish(),
+    cursor: z.number().nullish(), // <-- "cursor" needs to exist, but can be any type
+  }))
+  .query(async ({ input }) => {
+    const limit = input.limit ?? 50;
+    const { cursor } = input;
+    const items = await prisma?.site_info.findMany({
+      take: limit + 1, // get an extra item at the end which we'll use as next cursor
+      // where: {
+      //   title: {
+      //     contains: 'Prisma' /* Optional filter */,
+      //   },
+      // },
+      cursor: cursor ? { id: cursor } : undefined,
+      orderBy: {
+        id: 'asc',
+      },
+    })
+    let nextCursor: typeof cursor | undefined = undefined;
+
+    const totalDocs = await prisma?.site_info.count()
+
+
+    if (items!.length > limit) {
+      const nextItem = items!.pop()
+      nextCursor = nextItem!.id;
+    }
+    return {
+      items,
+      totalDocs,
+      nextCursor,
+    };
+  })
 });
